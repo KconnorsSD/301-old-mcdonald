f20c04285718bedc093c4691acd20eecaf241a62
Kevin <kevin_connors@intuit.com>
2022-03-09 21:51:10 +0000

    full replace on layout

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ mycolorscale = 'ylorrd' # Note: The error message will list possible color scale
 mycolorbartitle = "Millions USD"
 tabtitle = 'Old McDonald+'
 sourceurl = 'https://plot.ly/python/choropleth-maps/'
-githublink = 'https://https://github.com/KconnorsSD/agriculture-exports-map'
+githublink = 'https://github.com/KconnorsSD/agriculture-exports-map'
 
 
 ########## Set up the chart
@@ -52,16 +52,50 @@ app.title=tabtitle
 ########### Set up the layout
 
 app.layout = html.Div(children=[
-    html.H1(myheading1),
-    dcc.Graph(
-        id='figure-1',
-        figure=fig
-    ),
+    html.H1('2011 Agricultural Exports, by State'),
+    html.Div([
+        html.Div([
+                html.H6('Select a variable for analysis:'),
+                dcc.Dropdown(
+                    id='options-drop',
+                    options=[{'label': i, 'value': i} for i in list_of_columns],
+                    value='corn'
+                ),
+        ], className='two columns'),
+        html.Div([dcc.Graph(id='figure-1'),
+            ], className='ten columns'),
+    ], className='twelve columns'),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
     ]
 )
+
+
+# make a function that can intake any varname and produce a map.
+@app.callback(Output('figure-1', 'figure'),
+             [Input('options-drop', 'value')])
+def make_figure(varname):
+    mygraphtitle = f'Exports of {varname} in 2011'
+    mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
+    mycolorbartitle = "Millions USD"
+
+    data=go.Choropleth(
+        locations=df['code'], # Spatial coordinates
+        locationmode = 'USA-states', # set of locations match entries in `locations`
+        z = df[varname].astype(float), # Data to be color-coded
+        colorscale = mycolorscale,
+        colorbar_title = mycolorbartitle,
+    )
+    fig = go.Figure(data)
+    fig.update_layout(
+        title_text = mygraphtitle,
+        geo_scope='usa',
+        width=1200,
+        height=800
+    )
+    return fig
+
 
 ############ Deploy
 if __name__ == '__main__':
